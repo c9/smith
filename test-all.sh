399dc5a92a50a3681adf4688a904420ec639a202
Tim Caswell <tim@creationix.com>
2012-08-03 17:35:32 -0500

    Add more tests

--- test-all.sh
+++ test-all.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-node tests/test-memory-leaks.js && \
-node tests/test-agent.js && \
+echo "\nChecking message deframer..." && \
 node tests/test-framer.js && \
-node tests/test-scrubber.js 
+echo "\nChecking message scrubber..." && \
+node tests/test-scrubber.js && \
+echo "\nChecking Agent interface..." && \
+node tests/test-agent.js && \
+echo "\nChecking for memory leaks..." && \
+node tests/test-memory-leaks.js
